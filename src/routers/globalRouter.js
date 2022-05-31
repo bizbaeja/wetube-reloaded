4ab8720f8ce01efacc02ca386b3120244b0cd16f
bizbaeja <75407823+bizbaeja@users.noreply.github.com>
2022-05-31 16:04:29 +0900

    #4.6 강의 소스코드

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -6,5 +6,7 @@ const globalRouter = sexypress.Router();
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
