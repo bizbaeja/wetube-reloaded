c14344ea6dbe223b03c0cb273751b21df3e55ee5
bizbaeja <75407823+bizbaeja@users.noreply.github.com>
2022-06-06 12:53:15 +0900

    이벤트 다루기 (1)

---
+++ simpleroute/src/Event.js
@@ -0,0 +1,5 @@
+const resetbutton = document.querySelector('.reset');
+//스크립트를 따로 개발하는 시대이기 때문에 애드 이벤트 리스너를 쓴다는것은 무슨의미인가??
+resetbutton.addEventListener('click', function (event){
+    console.dir(event);
+});
